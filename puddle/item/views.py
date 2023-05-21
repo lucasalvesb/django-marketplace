c494b7baa5944abac31f0220abbc781472c64d06
Lucas Alves <lucasalves94@gmail.com>
2023-05-21 20:46:43 -0300

    new item dashboard  added

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, render
+
+from .forms import NewItemForm
 
 from .models import Item
 
@@ -7,3 +10,9 @@ def detail(request, pk):
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
 
     return render(request, 'item/detail.html', {'item': item, 'related_items': related_items})
+
+@login_required
+def new(request):
+    form = NewItemForm()
+
+    return render(request, 'item/form.html', {'form': form, 'title': 'New Item'})
