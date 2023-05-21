14a3681d7046a036dd39cfbbd4e7f9c72d0a3553
Lucas Alves <lucasalves94@gmail.com>
2023-05-20 22:18:57 -0300

    added categories and items as well as adjustes some configs

---
+++ puddle/item/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Category(models.Model):
+  name = models.CharField(max_length=255)
+
+  class Meta:
+    ordering = ('name',)
+    verbose_name_plural= "Categories"
+
+  def __str__(self):
+    return self.name
+  
+
+class Item(models.Model):
+  name = models.CharField(max_length=255)
+  description = models.TextField(blank=True, null=True)
+  price = models.FloatField()
+  is_sold = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
